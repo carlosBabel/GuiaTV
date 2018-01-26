859f4f61c5c131207bd480ae7ee0d148b3d6fcbf
CarlosSaboya <carlos.babel@gmail.com>
2018-01-26 13:27:57 +0100

    http get json

--- enlaces/script.js
+++ enlaces/script.js
@@ -5,9 +5,35 @@ var app = angular.module("enlace",['ngRoute'])
 		when('/',{
 			templateUrl: 'inicio.html',
 			controller:'InicioController'
-		});
+		}).
+		when('/bonus-malus',{
+			templateUrl: 'bonus-malus.html',
+			controller:'BonusController'
+		}).
+		when('/xml',{
+			templateUrl: 'xml.html',
+			controller:'XMLController'
+		}).
+		when('/cursos-online',{
+			templateUrl: 'cursos-online.html',
+			controller:'CursosController'
+		}).
+		otherwise({redirectTo:'/'})
 }])
 
-.controller('InicioController',[function(){
-		console.log('This is the main controller');
+.controller('InicioController',['$scope', function($scope){
+
+}]);
+.controller('BonusController',['$scope', function($scope){
+
+}]);
+
+.controller('XMLController',['$scope', '$http', function($scope, $http){
+	$http.get('enlaces.json')then(function(response){
+		$scope.enlaces = response.data;
+	}
+}]);
+
+.controller('CursosController',['$scope', function($scope){
+
 }]);
