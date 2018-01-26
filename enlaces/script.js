30ccd718c3cd7d7f4b7436b7f7adce152ad15343
CarlosSaboya <carlos.babel@gmail.com>
2018-01-26 12:07:13 +0100

    angular

---
+++ enlaces/script.js
@@ -0,0 +1,13 @@
+var app = angular.module("enlace",['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/#inicio',{
+			templateUrl: 'inicio.html',
+			controller:'InicioController'
+		});
+}])
+
+.controller('InicioController',[function(){
+		console.log('This is the main controller');
+}]);
