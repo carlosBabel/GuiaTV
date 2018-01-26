154a7257d278cbd58bd566fa864e84982016522d
CarlosSaboya <carlos.babel@gmail.com>
2018-01-26 14:09:34 +0100

    :+1:

--- enlaces/script.js
+++ enlaces/script.js
@@ -23,20 +23,20 @@ var app = angular.module("enlace",['ngRoute'])
 
 .controller('InicioController',['$scope', function($scope){
 
-}]);
-/*
+}])
+
 .controller('BonusController',['$scope', '$http', function($scope, $http){
 	$http.get('enlaces.json')then(function(response){
 		$scope.enlaces = response.data;
 	}
-}]);
+}])
 
 .controller('XMLController',['$scope', '$http', function($scope, $http){
 	$http.get('enlaces.json')then(function(response){
 		$scope.enlaces = response.data;
 	}
-}]);
-*/
+}])
+
 .controller('CursosController',['$scope', '$http', function($scope, $http){
 	$http.get('enlaces.json')then(function(response){
 		$scope.enlaces = response.data;
